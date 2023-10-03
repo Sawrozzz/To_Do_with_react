2b53dd4825a5e7db27505a52bd8b5908482bb47d
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-10-03 08:38:30 +0545

    my commit

--- src/Form.js
+++ src/Form.js
@@ -1,62 +1,61 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 
-function Form(){
-  const[task, setTask]= useState("");
+function Form() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
 
-  const[list, setList]= useState([]);
-  const handleTaskInput =(e)=>{
+  const handleTaskInput = (e) => {
     setTask(e.target.value);
-  }
-  const addTask=(e)=>{
-    if(task.trim() !== " "){
+  };
+
+  const addTask = () => {
+    if (task.trim() !== "") {
       const newTask = {
-        id:Date.now(),
-        text:task,
-        completed:false,
+        id: Date.now(),
+        text: task,
+        completed: false,
       };
-      setList([...list,
-        newTask
-      ]);
-      setTask(""); //to clear the input field
-    };
+      setList([...list, newTask]);
+      setTask(""); // Clear the input field
+    }
+  };
+
+  const toggleTask = (taskId) => {
+    setList((previousTasks) =>
+      previousTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
   };
 
   return (
-   <>
-   <div className="Forform">
-        <input type="text" name="" placeholder="Add your text Here.." value={task}
-        onChange = {handleTaskInput}
+    <>
+      <div className="Forform">
+        <input
+          type="text"
+          name=""
+          placeholder="Add your text Here.."
+          value={task}
+          onChange={handleTaskInput}
         />
-        <button className='btn' onClick={addTask}>+</button>
-      </div>
-      <input type="radio"
-      checked = {task.completed}
-   
-   </>
-  )
+        </div>
+      <ul className="list-container">
+        {list.map((task) => (
+          <li className="list" key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(task.id)}
+            />
+            {task.text}
+          </li>
+        ))}
+      </ul>
+      <button className="btn" onClick={addTask}>
+      +
+    </button>
+    </>
+  );
 }
 
-export default Form
-
-
-
-
-
-
-// function Form() {
-//   return (
-//     <>
-//       <div className="Forform">
-//         <input type="text" name="" placeholder="Add your text Here.."></input>
-//         <ul className="list-container">
-//           <li className="list">Task 1</li>
-//           <li className="list">Task 2</li>
-//           <li className="list">Task 3</li>
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Form;
+export default Form;
